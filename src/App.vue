f9346f1e18b636cd6ec81df5a2c708d108d50fc9
Treasure Porth <treasure.rose@gmail.com>
2022-04-10 16:02:46 -0700

    Add styling tweaks

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <template>
-  <header><h1>Wine Search</h1></header>
+  <header>
+    <h1>Find A Wine</h1>
+  </header>
   <ais-instant-search index-name="wine_records" :search-client="searchClient" class="container">
     <aside class="sidebar">
       <h2>Type</h2>
       <ais-refinement-list attribute="type"/>
-      <h2>Domain</h2>
+      <h2>Origin</h2>
       <ais-refinement-list attribute="domain"/> 
       <h2>Year</h2>
       <ais-refinement-list attribute="year"/> 
-      <ais-configure :hitsPerPage="6" />
+      <ais-configure :hitsPerPage="9" />
     </aside>
-    <main class="search-main">
+    <main>
       <ais-search-box />
       <ais-hits class="wine-results">
         <template v-slot:item="{ item }">
           <div class="wine-details">
             <img :src="item.image" :alt="item.name">
-            <h3>{{item.name}}</h3>
-            <h4>{{item.domain}}, {{item.year}}</h4>
+            <h3>{{item.name}}, {{item.year}}</h3>
+            <h4>{{item.domain}}</h4>
             <p>${{item.price}}</p>
           </div>
         </template>
@@ -44,7 +46,7 @@ export default {
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Open Sans',Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -57,16 +59,17 @@ export default {
   margin: auto;
   display: grid;
   grid-template-areas: 
-    "header header header header"
-    "aside main main main";
-}
-
-.search-main {
-  padding: 2em;
+    "header header header header header"
+    "aside aside main main main";
 }
 
 .sidebar {
   padding: 0 2em;
+  width: 100%;
+}
+
+.ais-SearchBox {
+  width: 95%;
 }
 
 .ais-Hits-list {
@@ -78,6 +81,19 @@ export default {
   li {
     flex-basis: calc((100% / 3) - 2em);
     justify-content: center;
+
+    &:hover {
+      box-shadow: 0 0 0 1px rgb(35 38 59 /20%), 0 1px 3px 0 rgb(35 38 59 / 70%);
+    }
+  }
+
+  h4 {
+    font-style: italic;
+  }
+
+  p {
+    font-weight: bold;
+    font-size: 1.2em;
   }
 }
 
