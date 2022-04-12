411e8b59fb2652f61374ee65fe20094ad8b18643
Treasure Porth <treasure.rose@gmail.com>
2022-04-12 11:55:46 -0700

    Updates

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import InstantSearch from 'vue-instantsearch/vue3/es';
-
-const app = createApp(App);
-
-app.use(InstantSearch);
-app.mount('#app');
